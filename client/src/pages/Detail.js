1bf40f5659a1c566f4291a7d4b4f52ec2b16f928
Todd Malone <toddomalone@gmail.com>
2021-05-14 16:06:38 -0500

    updated API file and calls

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
+import {Books as API} from "../utils/API";
 
 function Detail(props) {
   const [book, setBook] = useState({})
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,24 +1,29 @@
 import axios from "axios";
 
-export default Books= {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  const Books = {
+    // Gets all books
+    getBooks: function () {
+      return axios.get("/api/books");
+    },
+    // Gets the book with the given id
+    getBook: function (id) {
+      return axios.get("/api/books/" + id);
+    },
+    // Deletes the book with the given id
+    deleteBook: function (id) {
+      return axios.delete("/api/books/" + id);
+    },
+    // Saves a book to the database
+    saveBook: function (bookData) {
+      return axios.post("/api/books", bookData);
+    }
   }
-};
 
-export default Users ={
-  
-}
+  const Users = {
+    login: function ({ name, password }) {
+      return axios.post("/")
+    }
+  }
+
+
+export {Users, Books}
