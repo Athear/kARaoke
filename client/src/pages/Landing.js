4d8e76775e4f8d06e7803b371446b1ed10811b22
CaroKris <ckhilpisch@gmail.com>
2021-05-15 12:43:16 -0500

    reference to login in landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,7 +6,7 @@ function Landing() {
     <div>
       <h1 className="title">kARaoke</h1>
 
-      <a className="btn btn-primary btn-lg" href="#" role="button">
+      <a className="btn btn-primary btn-lg" href="/login" role="button">
         Learn more
       </a>
     </div>
