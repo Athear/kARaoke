c4510245a1915b92e5dcbcbac0cb70d015ce4074
CaroKris <ckhilpisch@gmail.com>
2021-05-15 13:17:27 -0500

    fixing API routes

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Jumbotron({ children }) {
   return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
-      {children}
+    <div>
+    {/* //   style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+    //   className="jumbotron"
+    // 
+    //   {children} */}
     </div>
   );
 }
