df7b79a1561078aee0d364074e47a8c45e0b7f9b
Todd Malone <toddomalone@gmail.com>
2021-05-15 22:03:27 -0500

    moved router declaration so sessions are registered correclty

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
+
 
 //add session storage
 const sess = {
@@ -26,6 +25,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+// Add routes, both API and view
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/karaoke"),
