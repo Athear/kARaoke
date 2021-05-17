66e212b7f60760a161fb90aade2e3884dd452a8c
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-05-16 19:57:41 -0500

    updated dependencies, refactored sketch.js, canvas and posenet now running on stage

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -1,20 +1,13 @@
 import React from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as posenet from '@tensorflow-models/posenet';
+import P5Wrapper from 'react-p5-wrapper'
+import sketch from "./sketch"
 import "./style.css";
 
-const videoWidth = 900;
-const videoHeight = 700;
-
-async function setupCamera(){
-  
-}
 
 const Canvas = () => (
 
-
   <div className="canvas">
-    What up?
+    <P5Wrapper sketch ={sketch}/>
   </div>
   
 );
---
+++ client/src/components/Canvas/sketch.js
@@ -0,0 +1,83 @@
+import * as ml5 from "ml5";
+
+// const net = await posenet.load()
+
+function sketch (p){
+let img1;
+let img2;
+let img3;    
+let video;
+let poseNet;
+let pose;
+
+
+let outfit = {
+  sunglasses: false,
+  hat: false,
+  shirt: false,
+};
+
+p.preload = function() {
+  img1 = p.loadImage("https://chriscastle.com/temp/chrisg/gangsta.png");
+  img2 = p.loadImage("https://www.chriscastle.com/temp/chrisg/cowboyHat.png");
+  img3 = p.loadImage("https://chriscastle.com/temp/chrisg/fishnetShirt.png");
+}
+
+p.setup = function () {
+   
+  p.createCanvas(1280, 1000);
+
+  video = p.createCapture(p.VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", gotPoses);
+  
+}
+
+function gotPoses(poses) {
+    // console.log(poses);
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+  }
+}
+
+function modelLoaded() {
+  console.log("poseNet ready");
+}
+
+
+p.draw = function() {
+  p.image(video, 0, 0);
+  if (outfit.sunglasses) {
+    p.image(img1, pose.rightEye.x - 135, pose.rightEye.y - 40);
+  }
+  if (outfit.hat) {
+    p.image(img2, pose.nose.x - 180, pose.nose.y - 230);
+  }
+  if (outfit.shirt) {
+    p.image(img3, pose.rightShoulder.x-100, pose.rightShoulder.y-100);
+  }
+  // if (pose) {
+  //   image(img, pose.rightEye.x-135, pose.rightEye.y-40);
+  // }
+}
+
+//
+// document.getElementById("btns").addEventListener("toggle", (e) => {
+
+//   mousePressed(e);
+
+// });
+
+
+function mousePressed(e) {
+  
+  outfit[e.target.id]= !outfit[e.target.id];
+  p.redraw();
+  console.log(outfit);
+}
+}
+// Here is the url for serving pics for dev http://www.chriscastle.com/temp/chrisg/ http://ftp.chriscastle.com/videos/yt1s.com%20-%20Miley%20Cyrus%20%20Wrecking%20Ball%20Karaoke%20Version.mp4
+//ftp.chriscastle.com/videos/
+
+export default sketch;
