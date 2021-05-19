03f4f8627293a8c83074a01ba6378ec30f513b7e
CaroKris <ckhilpisch@gmail.com>
2021-05-19 16:42:40 -0500

    adding screenshot capabilities

--- client/src/components/Canvas/sketch.js
+++ client/src/components/Canvas/sketch.js
@@ -5,9 +5,6 @@ function sketch (p, props){
 let img1;
 let img2;
 let img3;
-let img1A;
-let img2B;
-let img3C;
 let video;
 let poseNet;
 let pose;
@@ -17,11 +14,10 @@ let outfit = {
   hat: true,
   shirt: true,
 };
-
 p.preload = function() {
-  img1A= p.loadImage("https://chriscastle.com/temp/chrisg/aviators.png");
-  img2B = p.loadImage("https://www.chriscastle.com/temp/chrisg/cowboy.png");
-  img3C = p.loadImage("https://chriscastle.com/temp/chrisg/fishnetShirt.png");
+  img1 = p.loadImage("https://chriscastle.com/temp/chrisg/aviators.png");
+  img2 = p.loadImage("https://www.chriscastle.com/temp/chrisg/cowboy.png");
+  img3 = p.loadImage("https://chriscastle.com/temp/chrisg/fishnetShirt.png");
 }
 
 
@@ -99,34 +95,33 @@ p.draw = function() {
 
     console.log("var outfit: ", costume);
     if (costume) {
-      // outfit = {
-      //   glasses: true,
-      //   hat: true,
-      //   shirt: true,
+      outfit = {
+        glasses: true,
+        hat: true,
+        shirt: true,
+      };
+      // p.preload = function(){
+      // img1 = currentSong.costume.glasses;
+      // img2 = currentSong.costume.hat;
+      // img3 = currentSong.costume.shirt;
       // };
-      
-      img1 = costume.glasses;
-      img2 = costume.hat;
-      img3 = costume.shirt;
-      
-   
+    img1 = currentSong.costume.glasses;
+    img2 = currentSong.costume.hat;
+    img3 = currentSong.costume.shirt;
 
       // console.log("redraw img vars: ", img1, img2,img3);
     } 
     if (p.background) {
       // filter = true;
-      // console.log(p.background);
+      console.log(p.background);
       
     }
   };
-
- 
 
   p.draw = function () {
     p.image(video, 0, 0);
    
     if (pose) {
-      // console.log("in draw: ", img1, img2, img3)
       let nose = pose.keypoints[0].position;
       let eye1 = pose.keypoints[1].position;
       let eye2 = pose.keypoints[2].position;
@@ -134,20 +129,27 @@ p.draw = function() {
       let shoulder2 = pose.keypoints[6].position;
       let shirtScaleWidth = (shoulder1.x-shoulder2.x);
       let scale = (eye1.x - eye2.x) / 250;
-        if (img1) {
-          p.image(img1A, nose.x - 353 * scale, nose.y - 200 * scale, img1A.width * scale, img1A.height * scale);
+        if (outfit.sunglasses) {
+          p.image(img1, nose.x - 353 * scale, nose.y - 200 * scale, img1.width * scale, img1.height * scale);
         }
-        if (img2) {
-          p.image(img2B, pose.nose.x - 465 * scale, pose.nose.y - 650 * scale, (img2B.width + 300) * scale, (img2B.height + 100) * scale);
+        if (outfit.hat) {
+          p.image(img2, pose.nose.x - 465 * scale, pose.nose.y - 650 * scale, (img2.width + 300) * scale, (img2.height + 100) * scale);
         }
-        if (img3) {
-          p.image(img3C, pose.rightShoulder.x - 200 * scale, pose.rightShoulder.y - 400 * scale, shirtScaleWidth + 100, shirtScaleWidth);
+        if (outfit.shirt) {
+          p.image(img3, pose.rightShoulder.x - 200 * scale, pose.rightShoulder.y - 400 * scale, shirtScaleWidth + 100, shirtScaleWidth);
         }
     }
    
   };
-
+  // creating a button with a saveAsCanvas function to create and save a screenshot
+  const saveImageBtn = p.createElement('span','<i style="color:blueviolet;" class="fas fa-camera fa-3x"></i>');
+  saveImageBtn.position(20, 400);
+  saveImageBtn.mousePressed(saveAsCanvas);
  
+}
+
+function saveAsCanvas () {
+  p.save ("karaoke_canvas.png")
 }
 }
 // Here is the url for serving pics for dev http://www.chriscastle.com/temp/chrisg/ http://ftp.chriscastle.com/videos/yt1s.com%20-%20Miley%20Cyrus%20%20Wrecking%20Ball%20Karaoke%20Version.mp4
