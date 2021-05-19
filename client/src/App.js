3cef22c2b737ba72964e1a99c76c8e488f6649f2
Todd Malone <toddomalone@gmail.com>
2021-05-18 21:18:35 -0500

    added ProvideAuth context provider for all routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ProvideAuth } from "./utils/use-auth"
 import Stage from "./pages/Stage";
 import Landing from "./pages/Landing";
 import NoMatch from "./pages/NoMatch";
@@ -10,27 +11,29 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-        <Route exact path={["/"]}>
-            <Landing />
-          </Route>
-          <Route exact path={["/login"]}>
-          <Login className="background"/>
-          </Route>
-          <Route exact path={["/stage"]}>
-            <Stage />
-          </Route>
-          {/* <Route exact path="/stage/:id">
-            <Detail />
-          </Route> */}
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ProvideAuth>
+      <Router>
+        <div>
+          <Switch>
+          <Route exact path={["/"]}>
+              <Landing />
+            </Route>
+            <Route exact path={["/login"]}>
+            <Login className="background"/>
+            </Route>
+            <Route exact path={["/stage"]}>
+              <Stage />
+            </Route>
+            {/* <Route exact path="/stage/:id">
+              <Detail />
+            </Route> */}
+            <Route>
+              <NoMatch />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ProvideAuth>
   );
 }
 
