69cb8aeca67de494cecd2d3adc880c59b7ebd39c
CaroKris <ckhilpisch@gmail.com>
2021-05-20 21:05:28 -0500

    adding userdb seeds

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -59,3 +59,48 @@ db.Stage.remove({})
     console.error(err);
     process.exit(1);
   });
+
+
+  const userSeed = [
+    {
+      username: "carolyn_h",
+      password: "password12345",
+      email: "carolyn@carolyn.com"
+
+    },
+    {
+      username: "markki_m",
+      password: "password12345",
+      email: "markki@markki.com"
+
+    },
+    {
+      username: "chris_g",
+      password: "password12345",
+      email: "chris@chris.com"
+
+    },
+    {
+      username: "todd_m",
+      password: "password12345",
+      email: "todd@todd.com"
+
+    },
+    {
+      username: "jennifer_n",
+      password: "password12345",
+      email: "jen@jen.com"
+
+    },
+  ]
+
+  db.User.remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
