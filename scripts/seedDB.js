f3eb2c43d081b8bea2e9063f5a73a36eef16e4ff
CaroKris <ckhilpisch@gmail.com>
2021-05-20 19:58:29 -0500

    adding in an axios call to create a giphy on 404 pages

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
+// This file empties the stage collection and inserts the stages below
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/karaoke");
 
