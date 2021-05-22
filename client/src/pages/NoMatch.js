ad82e8abae9f38952c7d51956295ca6b34e74902
CaroKris <ckhilpisch@gmail.com>
2021-05-22 09:54:40 -0500

    working on giphy

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,70 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import axios from 'axios';
- 
-
-function NoMatch() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          
-            <h1>404 Page Not Found-Try Tuning your Station</h1>
-            <h1>
-              {/* <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span> */}
-            </h1>
-          
-        </Col>
-      </Row>
-    </Container>
-  );
-}
+import axios from "axios";
 
 // function NoMatch() {
-//   const [error, setError] = useState(null);
-//   const [isLoaded, setIsLoaded] = useState(false);
-//   const [items, setItems] = useState([]);
+//   return (
+//     <Container fluid>
+//       <Row>
+//         <Col size="md-12">
 
-//   // Note: the empty deps array [] means
-//   // this useEffect will run once
-//   // similar to componentDidMount()
-//   useEffect(() => {
-//     axios.get("https://dad-jokes.p.rapidapi.com/random/joke")
-//       .then(res => res.json())
-//       .then(
-//         (result) => {
-//           setIsLoaded(true);
-//           setItems(result);
-//           console.log(result);
-//         },
-//         // Note: it's important to handle errors here
-//         // instead of a catch() block so that we don't swallow
-//         // exceptions from actual bugs in components.
-//         (error) => {
-//           setIsLoaded(true);
-//           setError(error);
-//         }
-//       )
-//   }, [])
+//             <h1>404 Page Not Found-Try Tuning your Station</h1>
+//             <h1>
+//               {/* <span role="img" aria-label="Face With Rolling Eyes Emoji">
+//                 🙄
+//               </span> */}
+//             </h1>
 
-//   if (error) {
-//     return <div>Error: {error.message}</div>;
-//   } else if (!isLoaded) {
-//     return <div>Loading...</div>;
-//   } else {
-//     return (
-//       <ul>
-//         {items.map(item => (
-//           <li key={item.id}>
-//             {item.name} {item.price}
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   }
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
 // }
 
+function NoMatch() {
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    axios
+      .fetch(
+        "https://api.giphy.com/v1/gifs/random?api_key=EDFnIGDFop0J3dJnQid2JTnCB0KZteY5&tag=shocking&rating=pg-13"
+      ) // asynchronously load contents of the url
+      // return a Promise that resolves when res is loaded
+      .then((res) => res.json()) // call this function when res is loaded
+      // return a Promise with result of above function
+      .then((res) => {
+        // call this function when the above chained Promise resolves
+        this.setItems({
+          data: res,
+          setError: res.error || null,
+          loading: false,
+        });
+
+        return (
+          <Container>
+            <Row>
+              <Col size="md-6 sm-12">
+                <ul>
+                  {items.map((item) => (
+                    <li key={item.id}>
+                      {item.name} {item.price}
+                    </li>
+                  ))}
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+        );
+      });
+  });
+}
 export default NoMatch;
