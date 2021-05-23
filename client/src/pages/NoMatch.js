e14818df7a2ab5c8001881836076b32d2b86ce88
CaroKris <ckhilpisch@gmail.com>
2021-05-23 17:23:40 -0500

    adding a wrong giphy to the 404 page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,69 +1,49 @@
-import React from "react";
-import { Col, Row, Container } from "../components/Grid";
-import axios from 'axios';
- 
+import React, { useState, useEffect } from "react";
+import { Row, Container } from "../components/Grid";
+import axios from "axios";
+import "../css/nomatch.css";
 
 function NoMatch() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          
-            <h1>404 Page Not Found-Try Tuning your Station</h1>
-            <h1>
-              {/* <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span> */}
-            </h1>
-          
-        </Col>
-      </Row>
-    </Container>
-  );
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.giphy.com/v1/gifs/search?q=wrong&api_key=EDFnIGDFop0J3dJnQid2JTnCB0KZteY5&limit=1"
+      )
+      .then(
+        (result) => {
+          setItems(result.data.data);
+          console.log(result);
+        },
+        (error) => {
+          setError(error);
+        }
+      );
+  }, []);
+  console.log(items);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else {
+    return (
+      <Container>
+        <Row>
+          <Row>
+            <div className="giphyContainer">
+              <ul>
+                {items.map((item) => (
+                  <li key={item.id}>
+                    <img src={item.images.original.url}></img>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Row>
+        </Row>
+      </Container>
+    );
+  }
 }
-
-// function NoMatch() {
-//   const [error, setError] = useState(null);
-//   const [isLoaded, setIsLoaded] = useState(false);
-//   const [items, setItems] = useState([]);
-
-//   // Note: the empty deps array [] means
-//   // this useEffect will run once
-//   // similar to componentDidMount()
-//   useEffect(() => {
-//     axios.get("https://dad-jokes.p.rapidapi.com/random/joke")
-//       .then(res => res.json())
-//       .then(
-//         (result) => {
-//           setIsLoaded(true);
-//           setItems(result);
-//           console.log(result);
-//         },
-//         // Note: it's important to handle errors here
-//         // instead of a catch() block so that we don't swallow
-//         // exceptions from actual bugs in components.
-//         (error) => {
-//           setIsLoaded(true);
-//           setError(error);
-//         }
-//       )
-//   }, [])
-
-//   if (error) {
-//     return <div>Error: {error.message}</div>;
-//   } else if (!isLoaded) {
-//     return <div>Loading...</div>;
-//   } else {
-//     return (
-//       <ul>
-//         {items.map(item => (
-//           <li key={item.id}>
-//             {item.name} {item.price}
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
 
 export default NoMatch;
