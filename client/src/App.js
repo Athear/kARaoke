5ada18964262c0e3af7ab71ed08becc206a87b3e
Todd Malone <toddomalone@gmail.com>
2021-05-25 17:59:10 -0500

    cleaned up unused code and spacing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ProvideAuth } from "./utils/use-auth"
 import Stage from "./pages/Stage";
 import Landing from "./pages/Landing";
@@ -8,13 +8,10 @@ import Login from "./pages/Login";
 import PrivateRoute from "./components/PrivateRoute"
 
 
-
-
 function App() {
   return (
     <ProvideAuth>
       <Router>
-        <div>
           <Switch>
             <Route exact path={["/"]}>
               <Landing />
@@ -25,14 +22,10 @@ function App() {
             <PrivateRoute exact path={["/stage"]}>
               <Stage />
             </PrivateRoute>
-            {/* <Route exact path="/stage/:id">
-              <Detail />
-            </Route> */}
             <Route>
               <NoMatch />
             </Route>
           </Switch>
-        </div>
       </Router>
     </ProvideAuth>
   );
